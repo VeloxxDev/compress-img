29be2f251829ce929d3c7b7ec074834fd041a2dd
MOULIN <moulin@Malvoyannt.localdomain>
2025-12-27 11:43:11 +0100

    Initialisationof quantification

---
+++ include/quantification.h
@@ -0,0 +1,11 @@
+#ifndef QUANT
+#define QUANT
+
+
+/*
+ * @func
+ *
+ */
+
+void quantification(histo h,int* tab,int K);
+
---
+++ src/quantification.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct histo_iter {
+    int R, G;
+    cell current;
+} *histo_iter;
+
+void quantification(histo h,int* tab,int K) {
+    int i,j;
+
+    list = create_cell(h,h->)
+        
+
+}
